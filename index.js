8d02777220267ab1f895079a6e444447167cdac5
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-05-03 15:58:58 +0530

    modify recipee data

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const app = express()
-const cors = require('cors')
 const chefInfo =require('./data/chefInformation.json');
 const recipee =require('./data/chefRecipees.json');
 const port = 5000
+const cors = require('cors')
+app.use(cors())
 
 app.get('/chef', (req, res) => {
   res.send(chefInfo)
@@ -14,7 +15,7 @@ app.get('/recipee', (req, res) => {
 app.get('/recipee', (req, res) => {
   res.send(recipee)
 })
-app.use(cors())
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
