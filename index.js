e280c481bd871cd93eadab7562ed9defcee9f893
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-05-03 11:56:18 +0530

    data load and send response

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const chefInfo =require('./data/chefInformation.json');
+const recipee =require('./data/chefRecipees.json');
 const port = 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/chef', (req, res) => {
+  res.send(chefInfo)
+})
+app.get('/recipee', (req, res) => {
+  res.send(recipee)
+})
+app.get('/recipee', (req, res) => {
+  res.send(recipee)
 })
 app.use(cors())
 
