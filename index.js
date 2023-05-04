e8cd6848618528f73b35ff6c06bfdce6098714d8
devbiplabmistry <dev.biplabmistry@gmail.com>
2023-05-04 18:14:40 +0530

    (vercel data)

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express')
 const app = express()
+
 const chefInfo =require('./data/chefInformation.json');
-const recipee =require('./data/chefRecipees.json');
-const port = 5000
+const recipe =require('./data/chefRecipees.json');
 const cors = require('cors')
+const port = 5000
+
+
+
 app.use(cors())
 
 app.get('/chef', (req, res) => {
   res.send(chefInfo)
 })
 app.get('/recipee', (req, res) => {
-  res.send(recipee)
+  res.send(recipe)
 })
 app.get('/recipee/:id', (req, res) => {
   const id =req.params.id;
-  const selectedRecipee = recipee.find(r => r._id === id)
+  const selectedRecipee = recipe.find(r => r._id === id)
   res.send(selectedRecipee)
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
